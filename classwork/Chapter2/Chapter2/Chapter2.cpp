817bb2b33933b2560c8975e35bf98b0e02ab8482
Prizma Kunwar <prizma.kunwar@my.tccd.edu>
2025-09-02 21:37:09 -0500

    chapter 2 intro

---
+++ classwork/Chapter2/Chapter2/Chapter2.cpp
@@ -0,0 +1,36 @@
+// Your Name
+// Lab
+// COSC 1436 Fall 2025
+#include <iostream>
+
+int main()
+{
+    // Every statement in C++ must end with a semicolon
+    // statement = Anything hat does something in the language.
+ //std::cout << "Hello World" << " " << "Prizma ";
+    std::cout << "Hello World " << "Prizma " << std::endl;
+    std::cout << "I am " << 20 << " years old" << std::endl;
+
+    std::cout << "Hello World " << "Prizma\n";
+    std::cout << "Hello World\tPrizma";
+    std::cout << "Hello World \"Prizma\"";
+    std::cout << "C:\\Windows";
+    std::string name;
+    name = "Name";
+    std::cout << "Hello World " << name;
+
+//<< = output string operator//
+//"..." = string literal// literal means read only memory
+// The value 20 is integer literal
+// floating point literals - one or more digits + fraction
+// 3.14159 or 3E10 -3E10 3E-10  (it can besmall doesn't matter)
+//std::endl  = endline//
+//(preprocessor directive) # include <iostream>
+//Escape sequence - special meaning to the compiler
+// - starts with \ followed by a character
+// \n =newline
+// \t = tab
+// \" = double quote
+// \\ = slash
+// variable - Named memory location where data is stored ghththtt
+}
