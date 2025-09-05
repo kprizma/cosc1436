30b9aa993829966244864de27665d230b730ed5d
Prizma Kunwar <prizma.kunwar@my.tccd.edu>
2025-09-04 21:28:22 -0500

    variables and types and assignment

--- classwork/Chapter2/Chapter2/Chapter2.cpp
+++ classwork/Chapter2/Chapter2/Chapter2.cpp
@@ -18,6 +18,54 @@ int main()
     std::string name;
     name = "Name";
     std::cout << "Hello World " << name;
+    //var-decl ::= T id;
+    //type = defines whAT CAN BE STORED IN THE VARIATION AND IN WHAT FORMAT.
+    int age;
+    age = 20;
+    std::cout << "You are " << age << " years old";
+    //Area of rectangle calculator
+    // A= kw
+    int length;
+    int width;
+    //int length = 10, width = 20;  ( you can code as like this way too )
+    length = 10;
+    width = 20;
+    // uninitialized variable  - you must assign a  value to a variable before you read it.
+    int area;
+    
+   area = length * width;
+    std::cout << " Area of rectangle is " << area << std::endl;
+    // 2 different approaches to variable declarations
+    // block - all variables together
+    int radius, diameter;
+    std::string circleName;
+
+    //now i can use the variables
+    //inline  - variables are declared just before thayare used
+    //int radius, diameter;
+    // use radius,diameter
+    //std::string, diamter;
+    //use circlename 
+    double pi;
+    pi = 3.14159;
+    char letterGrade;
+    letterGrade = 'A';
+
+    bool ispassing;
+    ispassing = true;
+
+    length = 100;
+    width = 100;
+    // rather we can say length = width = 100;
+
+        // variables are always used as noun
+    //all identifier should start with the letter digits or underscore you will not need underscore n this class so dont use it.
+    // | a vertical bar indicates or.
+    // + means 1 or more
+    // you cant have a identifier as a keywords
+    // identifier must be unique no repeatation
+
+
 
 //<< = output string operator//
 //"..." = string literal// literal means read only memory
@@ -32,5 +80,6 @@ int main()
 // \t = tab
 // \" = double quote
 // \\ = slash
-// variable - Named memory location where data is stored ghththtt
+// variable - Named memory location where data is stored 
+   // asn_ ::= E = E   KNOWN AS LEFT VALUE AND RIGHT VALUE 
 }
