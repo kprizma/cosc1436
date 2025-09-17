bd2a1ba8c86c36d099c9a5b58295baeb38e8c27a
prizma <prizma.kunwar@my.tccd.edu>
2025-09-16 21:32:50 -0500

    lab1

---
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -0,0 +1,99 @@
+// Lab 1
+// Prizma Kunwar
+// COSC 1436 - Fall 2025
+
+#include <iostream>
+#include <string>
+void main()
+{
+    std::cout << " Lab 1 " << std::endl;
+    std::cout << " Prizma Kunwar " << std::endl;
+    std::cout << " COSC 1436 - Fall 2025 \n" << std::endl;
+
+    // prompting user for their name.
+    std::string name;
+    std::cout << " Please enter your name: ";
+    std::cin.ignore();
+    std::getline(std::cin, name);
+    std::cout << name << std::endl;
+
+    // prompting user for 4 lab gardes.
+    int lab1, lab2, lab3, lab4;
+    std::cout << " Please enter lab 1: ";
+    std::cin >> lab1;
+
+    std::cout << " Please enter lab 2: ";
+    std::cin >> lab2;
+
+    std::cout << " Please enter lab 3: ";
+    std::cin >> lab3;
+
+    std::cout << " Please enter lab 4: ";
+    std::cin >> lab4;
+
+    // prompting user for 3 exam garde.
+    int exam1, exam2, exam3;
+    std::cout << " Please enter exam 1: ";
+    std::cin >> exam1;
+
+    std::cout << " Please enter exam 2: ";
+    std::cin >> exam2;
+
+    std::cout << " Please enter exam 3: ";
+    std::cin >> exam3;
+
+    int participation, finalexam;
+    std::cout << " Please enter participation: ";
+    std::cin >> participation;
+
+    std::cout << " Please enter Final exam: ";
+    std::cin >> finalexam;
+
+    // calculating lab and exam average.
+    double labAverage = (lab1 + lab2 + lab3 + lab4) / 4.0;
+    double examAverage = (exam1, exam2, exam3) / 3.0;
+    double classAverage = (labAverage * 0.65) + (examAverage * 0.20) + (participation * 0.05) + (finalexam * 0.10);
+
+
+
+
+
+
+    // displaying lab grades.
+    std::cout <<  name << ", your lab grades are: " << std::endl;
+    std::cout << " lab1 = " << lab1 << std::endl;
+    std::cout << " lab2 = " << lab2 << std::endl;
+    std::cout << " lab3 = " << lab3 << std::endl;
+    std::cout << " lab4 = " << lab4 << std::endl;
+   
+
+
+
+    // displaying exam grades.
+    std::cout << name << ", your exam grade are: " << std::endl;
+    std::cout << " exam1 = " << exam1 << std::endl;
+    std::cout << " exam2 = " << exam2 << std::endl;
+    std::cout << " exam3 = " << exam3 << std::endl;
+    
+    // diaplaying other grades.
+    std::cout << name << ", your other grades are: " << std::endl;
+    std::cout << "Participation = " << participation << std::endl;
+    std::cout << "Final Exam = " << finalexam << std::endl;
+
+
+
+    // calculating and displaying the class average.
+    std::cout << name << ", your class grade is: ";
+    std::cout << " Lab Average (65%) = " << labAverage << " %" << std::endl;
+    std::cout << " Exam Average (20%) = " << examAverage << " %" << std::endl;
+    std::cout << " Participation (5%) = " << participation << " %" << std::endl;
+    std::cout << " Final Exam (10%) = " << finalexam << " %" << std::endl;
+    std::cout << " Class Average = " << classAverage << " %" << std::endl;
+
+
+
+
+
+
+} 
+
