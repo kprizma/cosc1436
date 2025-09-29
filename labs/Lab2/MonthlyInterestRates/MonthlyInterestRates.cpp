955076a748e98f52c5d45f5585048928c2981d8d
prizma <prizma.kunwar@my.tccd.edu>
2025-09-28 22:13:25 -0500

    Lab 2

---
+++ labs/Lab2/MonthlyInterestRates/MonthlyInterestRates.cpp
@@ -0,0 +1,13 @@
+// Lab 1
+// Prizma Kunwar
+// COSC 1436 - Fall 2025
+
+#include <iostream>
+void main()
+{
+    std::cout << "Lab 2" << std::endl;
+    std::cout << "Prizma Kunwar" << std::endl;
+    std::cout << "COSC 1436 - Fall 2025" << std::endl << std::endl;
+
+
+}
